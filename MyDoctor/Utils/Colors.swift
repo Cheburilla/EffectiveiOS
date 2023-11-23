48f5eb1f9ef33e8092594a43aab3ea8204a75595
Alexander Safronov <sanya.safron2003@gmail.com>
2023-11-23 07:11:02 -0800

    added utils

---
+++ MyDoctor/Utils/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  MyDoctor
+//
+//  Created by Александр Сафронов on 16.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Colors {
+    static let main = Color(red: 0.28, green: 0.58, blue: 1)
+    static let grayText = Color(red: 0.53, green: 0.59, blue: 0.73)
+    static let whiteBG = Color(red: 0.98, green: 0.98, blue: 0.98)
+    static let greeting = Color(red: 0.05, green: 0.11, blue: 0.2)
+    static let doctorTitle = Color(red: 0.8, green: 0.88, blue: 1)
+    static let darkBlack = Color(red: 0.05, green: 0.11, blue: 0.2)
+    static let rating = Color(red: 1, green: 0.69, blue: 0.32)
+    static let bottomTabBar = Color(red: 0.39, green: 0.71, blue: 1)
+    static let divider = Color(red: 0.96, green: 0.96, blue: 0.96)
+}
---
+++ MyDoctor/Utils/Sizes.swift
@@ -0,0 +1,29 @@
+//
+//  Paddings.swift
+//  MyDoctor
+//
+//  Created by Александр Сафронов on 20.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Paddings {
+    static let buttonMenu = CGFloat(24)
+}
+
+struct Sizes {
+    static let search = CGFloat(24)
+    static let buttonMenu = CGFloat(24)
+    static let avatar = CGFloat(56)
+    static let location = CGFloat(16)
+    static let underDoctorIcons = CGFloat(20)
+    static let underMainDoctorIcons = CGFloat(20)
+    static let doctorArrow = CGFloat(24)
+    static let tabIcon = CGFloat(24)
+    static let doctorAvatar = CGFloat(48)
+}
+
+struct Corners {
+    static let main = CGFloat(12)
+}
